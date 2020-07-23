69286248ba93d82b3ed42a6d1f5b0d2696770ebc
carrymaniac <838537276@qq.com>
2020-07-23 19:36:47 +0800

    [GuessGame] [henry] feature: add should_return_number_when_RandomAnswerGenerater_generate_given_nothing test

---
+++ src/main/java/example/RandomAnswerGenerater.java
@@ -0,0 +1,17 @@
+package example;
+
+/**
+ * @ProjectName: unit-test-comprehensive
+ * @Package: example
+ * @ClassName: RandomAnswerGenerater
+ * @Author: carrymaniac
+ * @Description:
+ * @Date: 2020/7/23 7:25 下午
+ * @Version:
+ */
+public class RandomAnswerGenerater implements AnswerGenerater {
+    @Override
+    public String generate() {
+        return "";
+    }
+}
---
+++ src/test/java/example/RandomAnswerGeneraterTest.java
@@ -0,0 +1,31 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @ProjectName: unit-test-comprehensive
+ * @Package: example
+ * @ClassName: RandomAnswerGeneraterTest
+ * @Author: carrymaniac
+ * @Description:
+ * @Date: 2020/7/23 7:20 下午
+ * @Version:
+ */
+public class RandomAnswerGeneraterTest {
+    @Test
+    void should_return_number_when_RandomAnswerGenerater_generate_given_nothing() {
+        //given
+        AnswerGenerater randomAnswerGenerater = new RandomAnswerGenerater();
+        //when
+        String actual = randomAnswerGenerater.generate();
+        //then
+        assertDoesNotThrow(()->{
+            Integer.valueOf(actual);
+        });
+    }
+}
